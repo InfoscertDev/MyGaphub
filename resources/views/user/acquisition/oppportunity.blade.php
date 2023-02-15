558392b7fb64bd4a066a5b5815df0b2b02e5a162
InfoscertDev <versatilekaywize94@gmail.com>
2023-02-15 07:52:47 +0100

    Acquisition Opportunity fixes

--- resources/views/user/acquisition/oppportunity.blade.php
+++ resources/views/user/acquisition/oppportunity.blade.php
@@ -2,11 +2,11 @@
 @section('content')
     <div class="wd-f bg-white py-3">
         <div class="elevation-3 m-2 b-rad-20">
-            <div class="gap-center-bg pt-2"> 
+            <div class="gap-center-bg pt-2">
                 <div class="b-rad-20 mt-1 cal-head hd-opt" id="authhead">
                     <div class="b-rad-20 overlay2">
                         <div class="disclaim text-center">
-                            <h1 class="list-opt">Acquisition Opportunities</h1>  
+                            <h1 class="list-opt">Acquisition Opportunities</h1>
                             <select name="opportunities" class="select-opportunity mt-2" id="" class="mt-2">
                                 <option value="" disabled>Business  Asset</option>
                                 <option value="" disabled>Risk Asset</option>
@@ -17,13 +17,13 @@
                         </div>
                     </div>
                 </div>
-            </div> 
-            <div class="gap-lists ml-0"> 
+            </div>
+            <div class="gap-lists ml-0">
                 <div class="asset-list asset-opt">
                     <div class="list-img img-right img-opt">
-                        <img src="{{ isset($acquisition[0]) ? $acquisition[0]->photo : asset('/assets/images/photomix-coyarsgz76866any101808.png') }}" alt=" " class="img img-responsive">
+                        <img src="{{ isset($acquisition[0]) && isset($acquisition[0]->photo)  ? $acquisition[0]->photo : asset('/assets/images/photomix-coyarsgz76866any101808.png') }}" alt=" " class="img img-responsive">
                     </div>
-                    <div class="list-body"> 
+                    <div class="list-body">
                         <h5 class="list-subtitle bold">Category:</h5>
                         <h4 class="list-explore bg-bot-4">{{ isset($acquisition[0]) ? $acquisition[0]->fullname : 'Real Estate Asset Program (REAP)'}}</h4>
                         <h5 class="">
@@ -39,18 +39,18 @@
                             <a href="{{ route('user.reap-opportunity', $asset) }}" class="card-link text-white"><button class="btn btn-pry px-4"> Explore</button></a>
                         </div>
                     </div>
-                </div> 
+                </div>
                 <div class="asset-list asset-opt">
                     <div class="list-img img-right img-opt">
-                        <img src="{{ isset($acquisition[1]) ? $acquisition[1]->photo : asset('/assets/images/piawsgxshsgsxszazxabay163752.png') }}" alt=" " class="img img-responsive">
-                    </div>  
-                    <div class="list-body"> 
+                        <img src="{{ isset($acquisition[1]) && isset($acquisition[1]->photo) ? $acquisition[1]->photo : asset('/assets/images/piawsgxshsgsxszazxabay163752.png') }}" alt=" " class="img img-responsive">
+                    </div>
+                    <div class="list-body">
                         <h5 class="list-subtitle bold">Category:</h5>
                         <h4 class="list-explore bg-bot-4">{{ isset($acquisition[1]) ? $acquisition[1]->fullname : 'Green Asset National Product (GANP)' }}</h4>
                         <h5 class="">
                             <span class="bold">Country: </span> {{ isset($acquisition[1]) ? $acquisition[1]->country : 'Multiple' }}
                         </h5>
-                        <h5 class=""> 
+                        <h5 class="">
                             <span class="bold mr-1">Minimum Capital: </span> {{ isset($acquisition[1]) ? $acquisition[1]->capital : '£500.00' }}
                         </h5>
                         <h5 class="">
@@ -61,8 +61,8 @@
                         </div>
                     </div>
                 </div>
-            
+
             </div>
         </div>
-    </div> 
-@endsection 
+    </div>
+@endsection
