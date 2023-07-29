036eba1247ffff9a2f5a91f5794348f06aa0be26
InfoscertDev <versatilekaywize94@gmail.com>
2023-07-29 11:39:49 +0100

    SEED fix

--- app/Http/Controllers/Web/SeedController.php
+++ app/Http/Controllers/Web/SeedController.php
@@ -215,6 +215,8 @@ class SeedController extends Controller
                                 //  >whereBetween('period', [$period, $period_end])
                                  ->where('allocation_id', $ids)->get();
 
+      info( [  array_column($record_spend->toArray(), 'amount'), $ids  ] )     ;
+
       $record_seed = array_sum(array_column($record_spend->toArray(), 'amount'));
 
       $periods = AllocationHelpers::averageSeedDetail($user)['periods'];
