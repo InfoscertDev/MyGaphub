b2ac4f86f847c16ef95b7e0a324d6c9411ceab7a
InfoscertDev <versatilekaywize94@gmail.com>
2024-03-25 17:02:40 +0100

    Test Url Link

--- resources/views/user/seed/allocation_summary.blade.php
+++ resources/views/user/seed/allocation_summary.blade.php
@@ -58,6 +58,7 @@
                             </div>
                         @endforeach
                     </div>
+                    <?php echo Request::url() ?>
                     @if($seed == 'savings')   <p class="my-3 ff-rob text-center" data-toggle="modal" data-target="#savingsAllocationModal" onclick="$('#savingsSummaryModal').modal('hide')">Add more</p> @endif
                     @if($seed == 'expenditure')   <p class="my-3 ff-rob text-center" data-toggle="modal" data-target="#expenditureAllocationModal" onclick="$('#savingsSummaryModal').modal('hide')">Add more</p> @endif
                     @if($seed == 'education')   <p class="my-3 ff-rob text-center" data-toggle="modal" data-target="#educationAllocationModal" onclick="$('#savingsSummaryModal').modal('hide')">Add more</p> @endif
