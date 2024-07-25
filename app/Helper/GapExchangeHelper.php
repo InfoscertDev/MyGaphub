323b192561cd675f055730e52e9c1c6bddc86a72
InfoscertDev <versatilekaywize94@gmail.com>
2024-07-25 19:19:27 +0100

    checker

--- app/Helper/GapExchangeHelper.php
+++ app/Helper/GapExchangeHelper.php
@@ -15,7 +15,7 @@ use App\Wheel\CashAccount;
 use App\FinicialCalculator as Calculator;
 use stdClass;
 
-class GapExchangeHelper 
+class GapExchangeHelper
 {
     public static function convert_currency($user, $target_currency, $money, $automated = 1){
         // Log::info([$target_currency]);
@@ -24,13 +24,13 @@ class GapExchangeHelper
         $manual_currencies = GapCurrency::where('user_id', $user->id)->first();
         $base_currency = ($calculator->currency) ? explode(" ", $calculator->currency)[1]: 'USD';
         $target_currency = ($target_currency) ? explode(" ",$target_currency)[1] : 'USD';
-       
+
         // CBase Currency and Target currency are the same
         if ($base_currency == $target_currency) {
-            $diffrential = 1;   
-        }else{  
+            $diffrential = 1;
+        }else{
             // check if account is automated or manuual
-            if($automated){ 
+            if($automated){
                 $current = json_decode($system_currencies->currencies);
                 $base = $current->EUR /  $current->$base_currency;
             }else{
@@ -38,22 +38,22 @@ class GapExchangeHelper
                 $base = 1;
             }
             $diffrential = ($current->$target_currency) ? $current->$target_currency * $base : 1;
-        } 
+        }
         // info(['Diffrencial', $money / $diffrential]);
-        $total = round(($money / $diffrential), 2);  
+        $total = round(($money / $diffrential), 2);
         return $total ;
     }
 
     public static function reap_favourite($user){
         $audit = Audit::where('user_id', $user->id)->first();
         $wheel = [];
-        if (!$audit->reap_favourite) { 
+        if (!$audit->reap_favourite) {
             $audit->reap_favourite = $wheel;
-        }else{ 
+        }else{
             $audit->reap_favourite = json_decode($audit->reap_favourite);
         }
         return (array)($audit->reap_favourite);
-    } 
+    }
 
     public static function paginate_favourite($reap_favourite,$pageNumber = 0){
         $perpage = 6;
@@ -79,9 +79,9 @@ class GapExchangeHelper
             array_unshift($wheel,$asset);
         }
         $audit->reap_favourite = json_encode($wheel);
-        $audit->save(); 
+        $audit->save();
         return $audit;
-    } 
+    }
 
     public static function removeReapFavourite($user, $asset){
         $reap_favourite = GapExchangeHelper::reap_favourite($user);
@@ -92,10 +92,10 @@ class GapExchangeHelper
             // }
             $key = array_search($asset, $reap_favourite );
             unset($reap_favourite[$key]);
-            $wheel = array_values($reap_favourite); 
+            $wheel = array_values($reap_favourite);
             // var_dump( $audit->reap_favourite, $key ,json_encode($wheel)); return;
             $audit->reap_favourite = json_encode($wheel) ;
-            $audit->save(); 
+            $audit->save();
         }
         return $audit;
     }
@@ -111,7 +111,7 @@ class GapExchangeHelper
         return (array)($audit->ganp_favourite);
     }
 
-   
+
 
     public static function addGanpFavourite($user, $asset){
         $audit = Audit::where('user_id', $user->id)->first();
@@ -126,9 +126,9 @@ class GapExchangeHelper
             array_unshift($wheel,$asset);
         }
         $audit->ganp_favourite = json_encode($wheel);
-        $audit->save(); 
+        $audit->save();
         return $audit;
-    } 
+    }
 
     public static function removeGanpFavourite($user, $asset){
         $ganp_favourite = GapExchangeHelper::ganp_favourite($user);
@@ -139,12 +139,12 @@ class GapExchangeHelper
             // }
             $key = array_search($asset, $ganp_favourite );
             unset($ganp_favourite[$key]);
-            $wheel = array_values($ganp_favourite); 
+            $wheel = array_values($ganp_favourite);
             // var_dump( $audit->ganp_favourite, $key ,json_encode($wheel)); return;
             $audit->ganp_favourite = json_encode($wheel) ;
-            $audit->save(); 
+            $audit->save();
         }
-        return $audit; 
+        return $audit;
     }
 
     public static function availabeleMortgages($user){
@@ -153,14 +153,14 @@ class GapExchangeHelper
         $primary_debt = Debt::where('user_id', $user->id)->first();
         // var_dump($primary_debt->isArchive);
         $primary_exist = ($primary_resident || $primary_debt->isArchive == 0) ? true : false;
-        $residential = WheelClass::primaryEquityDetails($user)['primary']; 
+        $residential = WheelClass::primaryEquityDetails($user)['primary'];
 
         $debt = Debt::where('user_id', $user->id)->where('equity_id', 0)->first();
         $calculator = Calculator::where('user_id', $user->id)->first();
         $mortgages = Mortgage::where('user_id', $user->id)->where('equity_id', 0)->where('isArchive', 0)
                             ->where('secured_against','!=','Primary Residential Home')->latest()->get();
         if($debt) $debt = GapExchangeHelper::switchToDebtAccount($debt, 'Debt', $calculator->currency);
-        
+
         if($debt)  $mortgages_available = [$debt];
         else $mortgages_available = [];
         foreach($mortgages as $mortgage) array_push($mortgages_available, $mortgage);
@@ -176,14 +176,15 @@ class GapExchangeHelper
         $system_currencies = GapCurrency::where('user_id', 0)->first();
         $calculator = Calculator::where('user_id', $user->id)->first();
         $user_currency = $calculator->currency;
-        $bcurrency = explode(" ",$user_currency)[1]; 
+        info($user_currency);
+        $bcurrency = explode(" ",$user_currency)[1];
         // $user_currency = explode(" ",$calculator->currency)[0];
- 
+
         if(!$manual_currencies){
             $manual_rates = json_decode($system_currencies->currencies);
             $manual_currencies = new GapCurrency();
             $manual_currencies->user_id = $user->id;
-            
+
             if($manual_rates->$bcurrency == $manual_currencies->base){
                 $base = 1;
             }else{
@@ -191,10 +192,10 @@ class GapExchangeHelper
             }
             foreach ($manual_rates as $key => &$rate) {
                 $manual_rates->$key = round(($rate * $base), 4);
-            }     
+            }
             $manual_currencies->base = $bcurrency;
             $manual_currencies->currencies = json_encode($manual_rates);
-            $manual_currencies->save(); 
+            $manual_currencies->save();
         }
         if($system_currencies){
             $current = json_decode($system_currencies->currencies);
@@ -205,12 +206,12 @@ class GapExchangeHelper
             }
             foreach ($current as $key => &$rate) {
                 $current->$key = round(($rate * $base), 4);
-            }     
+            }
             $system_currencies->currencies = json_encode($current);
         }
         return compact('user_currency','manual_currencies', 'system_currencies');
     }
-    
+
     public static function switchToCashAccount($account, $name, $currency){
         $account->account_name = $name;
         $account->account_purpose = GapExchangeHelper::autoCashPurpose($name);
@@ -218,7 +219,7 @@ class GapExchangeHelper
         $account->account_currency = $currency;
         return $account;
     }
- 
+
     public static function switchToCreditAccount($account, $name, $currency){
         $account->alias = $name;
         $account->account_currency = $currency;
@@ -229,7 +230,7 @@ class GapExchangeHelper
     public static function switchToDebtAccount($account, $name, $currency){
         $account->alias = $name;
         $account->id = -1;
-        $account->account_currency = $currency; 
+        $account->account_currency = $currency;
         $account->open_balance = $account->baseline;
         $account->current_balance = $account->current;
         $account->secured_against = 'Primary Residential Home';
@@ -246,7 +247,7 @@ class GapExchangeHelper
         $account->secured_against = '';
         return $account;
     }
-    
+
     private static function autoCashPurpose($name){
         $name = strtolower($name); $purpose = '';
         if($name == 'alpha'){
@@ -260,12 +261,12 @@ class GapExchangeHelper
     }
 
     public static function wheelKPIAccount($user, $currency, $archive = false){
-        if ($archive) {        
+        if ($archive) {
             $bespokes = BespokeKPI::where('user_id', $user->id)->where('isArchive', 1)->latest()->limit(7)->get();
         } else {
             $bespokes = BespokeKPI::where('user_id', $user->id)->where('isArchive', 0)->latest()->limit(7)->get();
         }
-        
+
         $cash = []; $liabilities = [];
         // Init Bespoke
         foreach($bespokes as $bespoke){
@@ -299,26 +300,26 @@ class GapExchangeHelper
             $bespoke->account_currency = $currency;
             $bespoke->account_alias = $bespoke->wheel->account_alias;
             $bespoke->account_location = $bespoke->wheel->fund;
-            $bespoke->target_date = $bespoke->wheel->target_date; 
-            // $bespoke->isAnalytics = $bespoke->wheel->isAnalytics; 
+            $bespoke->target_date = $bespoke->wheel->target_date;
+            // $bespoke->isAnalytics = $bespoke->wheel->isAnalytics;
             $bespoke->account_header = 'tahbhzjbhvaghvxghavgysvxtysghzvxstyghxgyxvsgyzvxghsbvsyuh';
         }
         return $bespokes;
     }
 
     public static function kpiToLiability($bespokes, $currency){
-        foreach($bespokes as $bespoke){  
+        foreach($bespokes as $bespoke){
             $bespoke->creditor_name = $bespoke->kpi_name;
             $bespoke->account_type = $bespoke->dept_types;
             $bespoke->account_type = $bespoke->dept_types;
             $bespoke->account_details = $bespoke->kpi_details;
-            $bespoke->pay_strategy = $bespoke->extra; 
+            $bespoke->pay_strategy = $bespoke->extra;
             $bespoke->interest_rate = $bespoke->dept_interest;
             $bespoke->account_currency = $currency;
             $bespoke->account_alias = $bespoke->wheel->account_alias;
-            $bespoke->target_date = $bespoke->wheel->target_date; 
-            $bespoke->periodical_pay = $bespoke->wheel->periodical_pay; 
-            // $bespoke->isAnalytics = $bespoke->wheel->isAnalytics; 
+            $bespoke->target_date = $bespoke->wheel->target_date;
+            $bespoke->periodical_pay = $bespoke->wheel->periodical_pay;
+            // $bespoke->isAnalytics = $bespoke->wheel->isAnalytics;
             $bespoke->account_header = 'lapakoihangbshjbsxhgbxuhxbshxbxujahnzoazjmsozklnsz';
         }
         return $bespokes;
@@ -331,7 +332,7 @@ class GapExchangeHelper
             $liability = Liability::where('user_id', $user->id)->where('isAnalytics',1)->latest()->get();
             $cash = GapExchangeHelper::cashToBespoke($cash);
             $liability = GapExchangeHelper::liabiliToBespoke($liability);
- 
+
             foreach($cash as $bespoke){ $wheel->push($bespoke);}
             foreach($liability as $bespoke){ $wheel->push($bespoke);}
         }
@@ -345,7 +346,7 @@ class GapExchangeHelper
             $bespoke->savings_purposes = $bespoke->account_purpose ;
             $bespoke->cash_keptin  = $bespoke->account_type ;
             $bespoke->kpi_details = $bespoke->account_details ;
-            $bespoke->pay = $bespoke->extra  ; 
+            $bespoke->pay = $bespoke->extra  ;
             $bespoke->bespoke_type = 'saveup';
             $bespoke->account_header = 'aznjzbhxjnsxjbnxhsjgczbhzbvcjhbxvnbjhjzcb';
         }
@@ -353,14 +354,14 @@ class GapExchangeHelper
     }
 
     public static function liabiliToBespoke($liabilities){
-        foreach($liabilities as $bespoke){  
+        foreach($liabilities as $bespoke){
             $bespoke->kpi_name = $bespoke->creditor_name ;
             $bespoke->dept_types = $bespoke->account_type ;
             $bespoke->dept_types = $bespoke->account_type ;
             $bespoke->kpi_details = $bespoke->account_details;
-            $bespoke->extra = $bespoke->extra  ; 
+            $bespoke->extra = $bespoke->extra  ;
             $bespoke->dept_interest = $bespoke->interest_rate ;
-            $bespoke->bespoke_type = 'dept'; 
+            $bespoke->bespoke_type = 'dept';
             $bespoke->account_header = 'skjnaznkszxjnszjnzjnzjnmhjzbnhjxvgyzbjhbxc';
         }
         return $liabilities;
@@ -368,9 +369,9 @@ class GapExchangeHelper
 
     public static function submitIncomeAllocation($user, $crd, $alo){
         $fin = CalculatorClass::finicial($user);
-        $income_detail = IncomeHelper::analyseIncome($user, $fin['portfolio'],false);   
+        $income_detail = IncomeHelper::analyseIncome($user, $fin['portfolio'],false);
         if($income_detail['portfolio_diff'] <= 0 && $crd == 'ajkmzxjkcnkfsnznnjksxnjnkcnjc' & $alo == 'azsjkhbdjcbjszbhjbxjhcbjbhhbjghdx'){
-            $success = true; 
+            $success = true;
             $audit = Audit::where('user_id', $user->id)->first();
             $audit->income_allocated = 1;
             $audit->save();
